d1dcac1d0975cc33ca4bb3040ec26479227d188b
github username <github email address>
2019-04-29 00:00:27 -0400

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,21 @@
 class Formatter {
-  //add static methods here
+  static capitalize(string) {
+    return string[0].toUpperCase() + string.slice(1)
+  }
+
+  static sanitize(string) {
+
+  }
+
+  static titleize(string) {
+    let result=''
+    let array=capitalize(string).then(string.split(' '))
+    for (let str of array)  {
+      if (str == ('the'||'a'||'an'||'but'||'of'||'and'||'for'||'at'||'by'||'from')) {
+      } else  {
+        result = result + ' ' + capitalize(str)
+      }
+    }
+    return result
+  }
 }
